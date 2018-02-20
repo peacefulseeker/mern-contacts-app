dae41d8b24496789d2a70e87f2e556d3c15dd09f
Alexey Vorobyov <a.vorobjovs@sehner.international>
2018-02-20 08:34:08 +0200

    Another Test for heroku #3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import ContactBox from './ContactBox';
 import './App.css';
 
 let port = 3100;
-let boxProps = { urlLocal: `http://localhost:${port}/api/contacts`, url: `:${port}/api/contacts/`, pollInterval: 2000 }
+let portHeroku = 9401;
+let boxProps = {
+  urlLocal: `http://localhost:${port}/api/contacts`,
+  url: `https://ancient-river-68531.herokuapp.com/:${portHeroku}/api/contacts/`,
+  pollInterval: 2000
+};
 ReactDOM.render(
   <ContactBox {...boxProps} />,
   document.getElementById('root')
